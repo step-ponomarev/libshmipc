1d43c55009ee0e17895c3110a3da13f9347c19c3
Stepan Ponomarev <stepan.ponomarev@corp.mail.ru>
2025-06-19 14:18:40 +0300

    added concurrent tests

--- tests/ipc_buffer_concurrency_test.cpp
+++ tests/ipc_buffer_concurrency_test.cpp
@@ -8,6 +8,22 @@
 #include <string.h>
 #include <thread>
 
+void delayed_produce(IpcBuffer *buf, const size_t from, const size_t to) {
+  for (size_t i = from; i < to;) {
+    void *dest;
+    IpcTransaction tx = ipc_buffer_reserve_entry(buf, sizeof(i), &dest);
+    if (tx.status != IPC_OK) {
+      continue;
+    };
+
+    std::this_thread::sleep_for(std::chrono::microseconds(10));
+    memcpy(dest, &i, sizeof(i));
+    ipc_buffer_commit_entry(buf, tx.entry_id);
+
+    i++;
+  }
+}
+
 void produce(IpcBuffer *buf, const size_t from, const size_t to) {
   for (size_t i = from; i < to;) {
     IpcStatus status = ipc_buffer_write(buf, &i, sizeof(size_t));
@@ -124,12 +140,45 @@ void test_multiple_writer_multiple_reader() {
   free(buf);
 }
 
+void test_delayed_multiple_writer_multiple_reader() {
+  const uint64_t size = ipc_buffer_allign_size(128);
+  const size_t total = 3000;
+
+  uint8_t mem[size];
+  IpcBuffer *buf = ipc_buffer_create(mem, size);
+
+  auto dest = std::make_shared<concurrent_set<size_t>>();
+  std::thread p1(delayed_produce, buf, 0, 1000);
+  std::thread p2(delayed_produce, buf, 1000, 2000);
+  std::thread p3(delayed_produce, buf, 2000, 3000);
+
+  std::thread consumer(consume, buf, total, dest);
+  std::thread consumer2(consume, buf, total, dest);
+  std::thread consumer3(consume, buf, total, dest);
+
+  p1.join();
+  p2.join();
+  p3.join();
+  consumer.join();
+  consumer2.join();
+  consumer3.join();
+
+  assert(dest->size() == total);
+  for (size_t i = 0; i < total; i++) {
+    assert(dest->contains(i));
+  }
+
+  free(buf);
+}
+
 int main() {
   run_test("single writer & single reader", &test_single_writer_single_reader);
   run_test("multiple writer & single reader",
            &test_multiple_writer_single_reader);
   run_test("multiple writer & multiple reader",
            &test_multiple_writer_multiple_reader);
+  run_test("multiple delayed writer & multiple reader",
+           &test_delayed_multiple_writer_multiple_reader);
 
   return 0;
 }
