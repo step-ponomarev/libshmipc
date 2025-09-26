7211c26a0f6771c7b2133d07dcd058d1c8103e2c
Stepan Ponomarev <step.ponomarev@gmail.com>
2025-09-26 22:02:02 +0300

    tmp

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,6 @@ set -e
 cd "$(dirname "$0")/.."
 rm -rf build
 
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX=dist
-cmake --build build
-cmake --install build
-
-ctest --test-dir build --output-on-failure
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DSHMIPC_BUILD_SHARED=ON -DSHMIPC_BUILD_STATIC=ON -DSHMIPC_BUILD_TESTS=ON -DSHMIPC_ENABLE_WERROR=ON
+cmake --build build -j
+ctest --test-dir build -VV -j1 --timeout 120
