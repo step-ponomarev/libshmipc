8858dbe2bad08971b53ad13b93b1011c90369ab2
Stepan Ponomarev <step.ponomarev@gmail.com>
2025-09-27 18:14:21 +0300

    check new CI

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,7 @@ set -e
 cd "$(dirname "$0")/.."
 rm -rf build
 
-cmake -S . -B build
+CC=$(brew --prefix)/bin/gcc-15 CXX=/usr/bin/clang++ \
+  cmake -S . -B build
 cmake --build build -j
 ctest --test-dir build --timeout 10
