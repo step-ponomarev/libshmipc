8cf95d869fb669b6fc257270e8292ff02417619c
Stepan Ponomarev <step.ponomarev@gmail.com>
2025-12-27 05:14:58 +0300

    added stdint

--- core/src/ipc_futex.h
+++ core/src/ipc_futex.h
@@ -3,9 +3,9 @@
 #include <stdatomic.h>
 #include <sys/errno.h>
 #include <time.h>
-#include <unistd.h>
+#include <stdint.h>
 
 int ipc_futex_wait(_Atomic uint32_t *addr, uint32_t expected,
                    const struct timespec *timeout);
 int ipc_futex_wake_one(_Atomic uint32_t *addr);
-int ipc_futex_wake_all(_Atomic uint32_t *addr);
+int ipc_wake_all(_Atomic uint32_t *addr);
