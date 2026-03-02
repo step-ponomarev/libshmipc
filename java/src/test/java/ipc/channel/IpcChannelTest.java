121486a1614a6473f8913d4f12f3bde1473e0faf
Stepan Ponomarev <step.ponomarev@gmail.com>
2026-03-02 23:10:43 +0300

    test 3

--- java/src/test/java/ipc/channel/IpcChannelTest.java
+++ java/src/test/java/ipc/channel/IpcChannelTest.java
@@ -80,60 +80,60 @@ public class IpcChannelTest {
             }
         }
     }
-//
-//    @Test(timeout = 60000)
-//    public void basicMultiProducerMultiConsumerTest() throws InterruptedException {
-//        final int count = 100_000;
-//        final long size = IpcChannel.getSuggestedSize(200);
-//        try (final Arena arena = Arena.ofShared();
-//             final ExecutorService exec = Executors.newVirtualThreadPerTaskExecutor()
-//        ) {
-//            final MemorySegment memory = arena.allocate(size);
-//            final AtomicInteger send = new AtomicInteger(0);
-//            final ConcurrentHashMap<String, Object> sendEntities = new ConcurrentHashMap<>();
-//            final String messageTemplate = "Message %d";
-//
-//            IpcChannel.init(memory, size).close();
-//            for (int i = 0; i < 2; i++) {
-//                IpcChannel producer = IpcChannel.attach(memory);
-//                exec.execute(() -> {
-//                    while (true) {
-//                        final int num = send.getAndIncrement();
-//                        if (num >= count) {
-//                            break;
-//                        }
-//
-//                        final String msg = messageTemplate.formatted(num);
-//                        while (true) {
-//                            if (producer.write(msg.getBytes(StandardCharsets.UTF_8)) == IpcStatus.IPC_STATUS_OK) {
-//                                sendEntities.put(msg, STUB);
-//                                break;
-//                            }
-//                        }
-//                    }
-//                });
-//            }
-//
-//            final ConcurrentHashMap<String, Object> receivedEntities = new ConcurrentHashMap<>();
-//            for (int i = 0; i < 10; i++) {
-//                IpcChannel consumer = IpcChannel.attach(memory);
-//                exec.execute(() -> {
-//                    while (receivedEntities.size() != count) {
-//                        byte[] readResult = consumer.read(Duration.ofSeconds(1));
-//                        if (readResult != null) {
-//                            String message = new String(readResult, StandardCharsets.UTF_8);
-//                            receivedEntities.put(message, STUB);
-//                        }
-//                    }
-//                });
-//            }
-//
-//            exec.shutdown();
-//            exec.awaitTermination(10, TimeUnit.SECONDS);
-//            Assert.assertEquals(sendEntities.keySet(), receivedEntities.keySet());
-//            Assert.assertEquals(count, sendEntities.size());
-//        }
-//    }
+
+    @Test(timeout = 60000)
+    public void basicMultiProducerMultiConsumerTest() throws InterruptedException {
+        final int count = 100_000;
+        final long size = IpcChannel.getSuggestedSize(200);
+        try (final Arena arena = Arena.ofShared();
+             final ExecutorService exec = Executors.newVirtualThreadPerTaskExecutor()
+        ) {
+            final MemorySegment memory = arena.allocate(size);
+            final AtomicInteger send = new AtomicInteger(0);
+            final ConcurrentHashMap<String, Object> sendEntities = new ConcurrentHashMap<>();
+            final String messageTemplate = "Message %d";
+
+            IpcChannel.init(memory, size).close();
+            for (int i = 0; i < 2; i++) {
+                IpcChannel producer = IpcChannel.attach(memory);
+                exec.execute(() -> {
+                    while (true) {
+                        final int num = send.getAndIncrement();
+                        if (num >= count) {
+                            break;
+                        }
+
+                        final String msg = messageTemplate.formatted(num);
+                        while (true) {
+                            if (producer.write(msg.getBytes(StandardCharsets.UTF_8)) == IpcStatus.IPC_STATUS_OK) {
+                                sendEntities.put(msg, STUB);
+                                break;
+                            }
+                        }
+                    }
+                });
+            }
+
+            final ConcurrentHashMap<String, Object> receivedEntities = new ConcurrentHashMap<>();
+            for (int i = 0; i < 10; i++) {
+                IpcChannel consumer = IpcChannel.attach(memory);
+                exec.execute(() -> {
+                    while (receivedEntities.size() != count) {
+                        byte[] readResult = consumer.read(Duration.ofSeconds(1));
+                        if (readResult != null) {
+                            String message = new String(readResult, StandardCharsets.UTF_8);
+                            receivedEntities.put(message, STUB);
+                        }
+                    }
+                });
+            }
+
+            exec.shutdown();
+            exec.awaitTermination(10, TimeUnit.SECONDS);
+            Assert.assertEquals(sendEntities.keySet(), receivedEntities.keySet());
+            Assert.assertEquals(count, sendEntities.size());
+        }
+    }
 //
 //    @Test(timeout = 1000)
 //    public void timeout() {
