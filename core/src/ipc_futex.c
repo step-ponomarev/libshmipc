4e36951816049dd0de95e3e9f9b72b1ddd425a71
Stepan Ponomarev <step.ponomarev@gmail.com>
2026-03-05 00:12:27 +0300

    works

--- core/src/ipc_futex.c
+++ core/src/ipc_futex.c
@@ -3,35 +3,22 @@
 
 #ifdef __APPLE__
 
-static uint32_t to_ulock_timeout_us(const struct timespec *timeout) {
-  if (timeout == NULL) {
-    return 0; // 0 = бесконечно (ok для случая "без таймаута")
-  }
-
-  uint64_t us = (uint64_t) timeout->tv_sec * 1000000ULL;
-  us += ((uint64_t) timeout->tv_nsec + 999ULL) / 1000ULL;
-
-  if (us == 0) {
-    us = 1;
-  }
-
-  if (us > UINT32_MAX) {
-    us = UINT32_MAX;
-  }
-
-  return (uint32_t) us;
-}
-
 extern int __ulock_wait(uint32_t operation, void *addr, uint64_t value,
                         uint32_t timeout);
 extern int __ulock_wake(uint32_t operation, void *addr, uint64_t wake_value);
 
+static uint32_t to_ulock_timeout_us(const struct timespec *timeout);
 #define UL_COMPARE_AND_WAIT 1
 #define ULF_WAKE_ALL 0x00000100
 
 int ipc_futex_wait(_Atomic uint32_t *addr, uint32_t expected,
                    const struct timespec *timeout) {
-  int res = __ulock_wait(UL_COMPARE_AND_WAIT, addr, expected, to_ulock_timeout_us(timeout));
+  const uint32_t timeout_ns = to_ulock_timeout_us(timeout);
+  if (timeout_ns == 0) {
+    return 0;
+  }
+
+  int res = __ulock_wait(UL_COMPARE_AND_WAIT, addr, expected, timeout_ns);
   if (res != 0) {
     // ENOENT: value changed before we slept (compare failed) - this is normal
     // EINTR: interrupted by signal - continue waiting
@@ -64,6 +51,25 @@ int ipc_futex_wake_all(_Atomic uint32_t *addr) {
   return res;
 }
 
+static uint32_t to_ulock_timeout_us(const struct timespec *timeout) {
+  if (timeout == NULL) {
+    return 0;
+  }
+
+  uint64_t us = (uint64_t)timeout->tv_sec * 1000000ULL;
+  us += (uint64_t)timeout->tv_nsec / 1000ULL;
+
+  if (us == 0) {
+    us = 0;
+  }
+
+  if (us > UINT32_MAX) {
+    us = UINT32_MAX;
+  }
+
+  return (uint32_t)us;
+}
+
 #elif defined(__linux__)
 #include <limits.h>
 #include <linux/futex.h>
@@ -72,7 +78,7 @@ int ipc_futex_wake_all(_Atomic uint32_t *addr) {
 
 int ipc_futex_wait(_Atomic uint32_t *addr, uint32_t expected,
                    const struct timespec *timeout) {
-  int res = syscall(SYS_futex, addr, FUTEX_WAIT, expected,  to_ulock_timeout_us(timeout));
+  int res = syscall(SYS_futex, addr, FUTEX_WAIT, expected, timeout);
   if (res != 0) {
     // EAGAIN: value changed before we slept - this is normal, continue loop
     // EINTR: interrupted by signal - continue waiting
